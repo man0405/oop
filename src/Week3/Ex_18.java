034911bcc5363df822e1917605b944dece99b182
man0405 <vanman160504@gmail.com>
2022-10-29 18:19:22 +0700

    18

---
+++ src/Week3/Ex_18.java
@@ -0,0 +1,8 @@
+package Week3;
+
+public class Ex_18 {
+    public static void main(String[] args) {
+        System.out.println("1,7,16,37,79,173,...");
+        System.out.println("Em chịu chết");
+    }
+}
