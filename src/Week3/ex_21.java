3005176dee3f988a8bb8ed6de1b6a11866bc46ee
man0405 <vanman160504@gmail.com>
2022-10-31 09:59:22 +0700

    fix convert

--- src/Week3/ex_21.java
+++ src/Week3/ex_21.java
@@ -5,19 +5,17 @@ public class ex_21 {
     public static String conver(int number){
         char[] numberHexaDecimal = {'0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9', 'A' , 'B' , 'C' , 'D', 'E' , 'F'};
         String base = "";
-        if (number == 0 ) base += numberHexaDecimal[0];
+        if (number == 0 ) base = numberHexaDecimal[0] + base;
         if (number <0 ) {
             base += "-";
             number = -number;
         }
         while ( number != 0){
-            base += numberHexaDecimal[number%16];
+            base = numberHexaDecimal[number%16] + base;
             number /= 16;
         }
 
-        StringBuilder str = new StringBuilder(base);
-
-        return str.reverse().toString();
+        return base;
     }
 
     public static void main(String[] args) {
